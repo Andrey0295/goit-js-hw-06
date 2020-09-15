528aba8f9a361c8d08df7070a7256e0dfb0f8eb4
Andrey0295 <andreevicandrej00@gmail.com>
2020-09-15 18:42:29 +0300

    working with h/w 6

--- js/task-3.js
+++ js/task-3.js
@@ -9,30 +9,7 @@
 // 1.Отфильтровать по полу.
 // 2.Получить массив имен.
 
-// Write code under this line
-
-// const users = [
-//   { name: 'Mango', job: 'waiter', gender: 'male' },
-
-//   { name: 'Kiwi', job: 'teacher', gender: 'male' },
-
-//   { name: 'Poly', job: 'dentist', gender: 'female' },
-// ];
-// console.log(users);
-
-// const getName = (array, genders) =>
-//   array.filter(({ gender }) => gender === genders).map(({ name }) => name);
-
-// console.log(getName(users, 'male'));
-
 const getUsersWithGender = (array, genders) =>
   array.filter(({ gender }) => gender === genders).map(({ name }) => name);
 
 console.log(getUsersWithGender(users, 'male'));
-
-/* [
-  "Moore Hensley",
-  "Ross Vazquez",
-  "Carey Barr",
-  "Blackburn Dotson"
-] */
